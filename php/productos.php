2e2c5a9bd7b23f04799184120d4ecdba5032b0f5
Jorge Cabrera <jorge.cabrera.sanchez1@gmail.com>
2018-01-10 23:09:38 +0100

    Terminado merge

--- php/productos.php
+++ php/productos.php
@@ -2,10 +2,10 @@
 $datos=[];
 $id=$_GET['key'];
 $conn=mysqli_connect("localhost","root","root","bdtruekup");
-$sql="SELECT * FROM productos where idSubcategoria='$id'";
+$sql="SELECT id,nombre,descripcion,imagen FROM productos where idSubcategoria='$id'";
 $resultado=mysqli_query($conn,$sql);
 while($row=mysqli_fetch_array($resultado)){
-    array_push($datos,array("id"=>$row['id'],"idCategoria"=>$row['idSubcategoria'], "nombre"=> $row['nombre'] ,"descripcion"=>$row['descripcion'], "imagen"=>$row['imagen']));
+    array_push($datos,array("id"=>$row['id'], "nombre"=> $row['nombre'] ,"descripcion"=>$row['descripcion'], "imagen"=>$row['imagen']));
 }
 header('Content-type: application/json; charset=utf-8');
 echo json_encode($datos);
